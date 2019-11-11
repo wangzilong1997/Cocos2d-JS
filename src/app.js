cb2e21ccbec155f1fa770dca0700cdac0373b415
wangzilong1997 <1321787581@qq.com>
2019-11-12 07:56:53 +0800

    play about high

--- src/app.js
+++ src/app.js
@@ -1,44 +1,35 @@
 
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
-    ttfLabel:null,
     ctor:function () {
+        //////////////////////////////
+        // 1. super init first
         this._super();
         var size = cc.winSize;
+        this.addChild(new cc.LayerColor(cc.color.WHITE));
 
-        var startMenuItem = new cc.MenuItemFont("开始",function () {
-            cc.log("开始按钮被点击")
-        },this);
-        var setMenuItem = new cc.MenuItemFont("设置",function(){
-            cc.log("设置按钮点击了！");
-        },this);
+        var playMenuItem = new cc.MenuItemImage(res.PlayBtnNormal_png, res.PlaytBtnSelected_png, function () {
+            cc.log("playMenuItem");
+        }, this);
 
-        var ttfLabel = new cc.LabelTTF("其他","",30);
-        ttfLabel.setFontFillColor(cc.color.RED);
-        ttfLabel.enableStroke(cc.color.YELLOW,2);
-        ttfLabel.enableShadow(cc.color.WHITE,15,15,15);
+        var aboutMenuItem = new cc.MenuItemImage(res.AboutBtnNormal_png, res.AboutBtnSelected_png, function () {
+            cc.log("aboutMenuItem");
+        }, this);
 
-        var otherMenuItem = new cc.MenuItemLabel(ttfLabel,function(){
-            cc.log("其他按钮点击了！");
-        },this);
-        var menu = new cc.Menu(startMenuItem,setMenuItem,otherMenuItem);
+        var highMenuItem = new cc.MenuItemImage(res.HightScoresBtnNormal_png, res.HighScoresBtnSelected_png, function () {
+            cc.log("highMenuItem");
+        }, this);
+
+        var menu = new cc.Menu(playMenuItem, aboutMenuItem, highMenuItem);
         this.addChild(menu);
-        menu.x = 0;
-        menu.y = 0;
 
-        startMenuItem.x = size.width*0.5;
-        startMenuItem.y = size.height*0.5;
-
-        setMenuItem.x = size.width*0.2;
-        setMenuItem.y = size.height*0.3;
-
-        otherMenuItem.x = size.width*0.8;
-        otherMenuItem.y = size.height*0.7;
+        menu.alignItemsHorizontallyWithPadding(50);
+        // menu.setPosition(cc.p(0,0));
 
 
         return true;
+
     }
-       
 });
 
 var HelloWorldScene = cc.Scene.extend({
